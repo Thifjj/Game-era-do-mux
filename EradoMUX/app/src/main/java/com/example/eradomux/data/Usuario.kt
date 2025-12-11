b85481a15e68aeeb9cb0c3c43f74a9b178adca2f
Thiago Fernades Jacques <thifj.fj@gmail.com>
2025-12-10 21:58:05 -0300

    Delete EradoMUX directory

--- EradoMUX/app/src/main/java/com/example/eradomux/data/Usuario.kt
+++
@@ -1,9 +0,0 @@
-package com.example.eradomux.data
-
-data class Usuario(
-    val id: String = "", // ID do documento no Firestore
-    val nome: String = "",
-    val senha: String = "",
-    val nivel: Int = 1,
-    val professor : Boolean = false,
-)
